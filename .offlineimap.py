581e1d72c91ca895144f4c9960753595dd205da0
mrmanner <johan.manner@scouterna.se>
2017-11-21 16:54:48 +0100

    Use keepassrc for password management in offlineimap

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,43 +1,25 @@
 #! /usr/bin/env python2
 
-import gnomekeyring as gkey
+import os, getpass
+import libkeepass
 
-class Keyring(object):
-    def __init__(self, name, server, protocol):
-        self._name = name
-        self._server = server
-        self._protocol = protocol
-        self._keyring = gkey.get_default_keyring_sync()
-
-    def has_credentials(self):
-        try:
-            attrs = {"server": self._server, "protocol": self._protocol}
-            items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
-            return len(items) > 0
-        except gkey.DeniedError:
-            return False
-
-    def get_credentials(self):
-        attrs = {"server": self._server, "protocol": self._protocol}
-        items = gkey.find_items_sync(gkey.ITEM_NETWORK_PASSWORD, attrs)
-        return (items[0].attributes["user"], items[0].secret)
-
-    def set_credentials(self, (user, pw)):
-        attrs = {
-                "user": user,
-                "server": self._server,
-                "protocol": self._protocol,
-            }
-        gkey.item_create_sync(gkey.get_default_keyring_sync(),
-                gkey.ITEM_NETWORK_PASSWORD, self._name, attrs, pw, True)
-
-def get_username(server):
-    keyring = Keyring("offlineimap", server, "imap")
-    (username, password) = keyring.get_credentials()
-    return username
-
-def get_password(server):
-    keyring = Keyring("offlineimap", server, "imap")
-    (username, password) = keyring.get_credentials()
-    return password
+def get_keepass_pw(dbpath, title="", username=""):
+    if os.path.isfile(dbpath):
+        with libkeepass.open(
+            os.path.expanduser(dbpath),
+            password=getpass.getpass(
+                "Master password for '" + dbpath + "': ")) as kdb:
+            entry = kdb.tree.xpath(
+                './/Entry'
+                                '/String/Key[.="Title"]/../Value[.="{title}"]/../..'
+                                '/String/Key[.="UserName"]/../Value[.="{username}"]/../..'
+                                '/String/Key[.="Password"]/../Value'.format(
+                                    title=title,
+                                                        username=username
+                                                    )
+                            )[0]
+            return entry.text
+    else:
+        print "Error: Bad input filename."
+        return
 
