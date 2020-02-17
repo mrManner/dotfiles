fe25494f06e0b76389a73e474ec4ef5684f41d79
mrmanner <johan@mrmanner.eu>
2020-02-17 18:19:49 +0100

    Better fish PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,14 @@
-set -gx PATH /home/jmanner/go/bin $PATH
-
 . ~/.config/fish/gnupg.fish
+
+set -gx PATH $PATH ~/.local/bin $PATH 
+set -gx PATH $PATH (go env GOPATH) 
+set -gx PATH $PATH ~/projekt/tink-backend/bazel-bin/src/tink-integration-eidas-secrets-service/tink_secret_utils
+. ~/software/google-cloud-sdk/path.fish.inc
 
 fundle plugin 'danhper/fish-ssh-agent'
 fundle init
 
 # I like sudo !!
-
 function sudo
     if test "$argv" = !!
         eval command sudo $history[1]
