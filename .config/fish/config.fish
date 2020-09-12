1239505b552cfcfba1fd96587759572b73a1d510
mrmanner <johan@mrmanner.eu>
2020-09-12 23:18:17 +0200

    Better PATH modification

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 . ~/.config/fish/gnupg.fish
-
-set -gx PATH $PATH ~/.local/bin $PATH 
-set -gx PATH $PATH (go env GOPATH) 
 . ~/software/google-cloud-sdk/path.fish.inc
+
+set -gpx PATH /usr/local/bin
+set -gax PATH (go env GOPATH)/bin 
+set -gax PATH ~/.local/bin 
 
 fundle plugin 'danhper/fish-ssh-agent'
 fundle init
