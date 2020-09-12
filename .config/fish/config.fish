65f06cdf0d0b623b3fd6112eb6310d868a192da2
mrmanner <johan@mrmanner.eu>
2020-09-12 23:17:55 +0200

    Remove Tink-specific entries from PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@
 
 set -gx PATH $PATH ~/.local/bin $PATH 
 set -gx PATH $PATH (go env GOPATH) 
-set -gx PATH $PATH ~/projekt/tink-backend/bazel-bin/src/tink-integration-eidas-secrets-service/tink_secret_utils
 . ~/software/google-cloud-sdk/path.fish.inc
 
 fundle plugin 'danhper/fish-ssh-agent'
