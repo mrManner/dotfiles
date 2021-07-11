333f875eff167da84756a1442571bb708c70f709
mrmanner <johan@mrmanner.eu>
2021-07-11 19:54:53 +0200

    fish: even better path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 . ~/.config/fish/gnupg.fish
-set -gx PATH /home/jmanner/.local/bin /home/jmanner/go/bin $PATH
+
+fish_add_path ~/.local/bin
+fish_add_path /usr/local/bin
+fish_add_path /home/jmanner/go/bin
 
 fundle plugin 'danhper/fish-ssh-agent'
 fundle plugin 'jorgebucaran/nvm.fish'
