cbab061734610aa3389f41658db185f82a59f992
mrmanner <johan@mrmanner.eu>
2023-11-07 14:52:07 +0100

    sway: improve status line

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -6,13 +6,17 @@ uptime=$(uptime -p)
 # like 2018-10-06 and the time (e.g., 14:01)
 date_formatted=$(date "+%a %F %H:%M")
 
+# user and hostname so we know where we are
+user=$(whoami)
+host=$(hostnamectl hostname)
+
 # Returns the battery status: "Full", "Discharging", or "Charging".
-battery_charge=$(cat /sys/class/power_supply/BAT0/charge_now)
-battery_chargea=$(( 100 * battery_charge))
-battery_full=$(cat /sys/class/power_supply/BAT0/charge_full)
-battery=$((battery_chargea / battery_full))
+#battery_charge=$(cat /sys/class/power_supply/BAT0/charge_now)
+#battery_chargea=$(( 100 * battery_charge))
+#battery_full=$(cat /sys/class/power_supply/BAT0/charge_full)
+#battery=$((battery_chargea / battery_full))
 
 # Emojis and characters for the status bar
 # 💎 💻 💡 🔌 ⚡ 📁 \|
-echo "$uptime | battery $battery% charged | $date_formatted"
+echo "$user@$host | $uptime | $date_formatted"
 
