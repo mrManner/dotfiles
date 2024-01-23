e4f68f85e2b6fe2d7306b659815278f2414cadd4
mrmanner <johan@mrmanner.eu>
2024-01-23 12:58:19 +0100

    nvim: disable indentation guides
    They don't work unless "scope" is enabled, and that requires some
    colorscheme work else it looks horrible.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -181,19 +181,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {
-      scope = {
-        enabled = false
-      },
-    },
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
