0921b844a39b7a6fd44d41d7dae2aba1303f55ee
mrmanner <johan@mrmanner.eu>
2024-01-23 22:14:58 +0100

    nyxt: use duck duck go

--- nyxt/.config/nyxt/config.lisp
+++ nyxt/.config/nyxt/config.lisp
@@ -3,3 +3,14 @@
    ((default-modes
      (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
 
+;;; Set up search engines
+(defvar *search-engine-configs* (list '("ddg"
+                                        "https://duckduckgo.com/search?q=~a"
+                                        "https://duckduckgo.com") '("doi"
+                                        "https://dx.doi.org/~a"
+                                        "https://dx.doi.org")) "My search
+  engines") (define-configuration context-buffer "Make search engines of all
+  objects in the list of search engine configs" ((search-engines (append
+  (mapcar (lambda (engine-config) (apply 'make-search-engine engine-config))
+          *search-engine-configs*) %slot-default%))))
+
