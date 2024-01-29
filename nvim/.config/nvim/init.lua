cb6617816e9ee4deff138f7b50cd4a92a2c794f0
mrmanner <johan@mrmanner.eu>
2024-01-30 00:58:22 +0100

    nvim: add copilot plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -214,6 +214,11 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  -- Github copilot
+  "github/copilot.vim",
+
+
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -570,6 +575,37 @@ mason_lspconfig.setup_handlers {
 require'lspconfig'.racket_langserver.setup{
   on_attach = on_attach
 }
+
+-- configure copilot maps
+vim.keymap.set('i', '<C-q>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
+-- set up goimports formatting
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
+})
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
