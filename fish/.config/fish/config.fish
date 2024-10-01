af8f1f0130f049c75919662f1c4f08279f421200
mrmanner <johan@mrmanner.eu>
2024-10-01 08:24:47 +0200

    fish: add some more fish functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,3 +21,17 @@ set -x YVM_DIR /home/jmanner/.yvm
 [ -r $YVM_DIR/yvm.fish ]; and source $YVM_DIR/yvm.fish
 
 pyenv init - | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /usr/bin/conda
+    eval /usr/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/usr/etc/fish/conf.d/conda.fish"
+        . "/usr/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/usr/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
---
+++ fish/.config/fish/functions/logtodate.fish
@@ -0,0 +1,4 @@
+function logtodate --wraps='echo 1748828418' --description 'alias logtodate echo 1748828418'
+  echo 1748828418 $argv
+        
+end
