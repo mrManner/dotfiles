abdc5fe4b7730bb7b8dced94f2066dd259668468
mrmanner <johan@mrmanner.eu>
2024-10-01 08:24:05 +0200

    sway: remove broken battery status

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -12,16 +12,8 @@ host=$(hostnamectl hostname)
 
 # Returns the battery status: "Full", "Discharging", or "Charging".
 bat0_status=$(cat /sys/class/power_supply/BAT0/status)
-bat0_charge=$(cat /sys/class/power_supply/BAT0/energy_now)
-bat1_charge=$(cat /sys/class/power_supply/BAT1/energy_now)
-bat0_full=$(cat /sys/class/power_supply/BAT0/energy_full)
-bat1_full=$(cat /sys/class/power_supply/BAT1/energy_full)
-battery_charge=$((bat0_charge + bat1_charge))
-battery_full=$((bat0_full + bat1_full))
-battery=$((100 * battery_charge / battery_full))
-
 
 # Emojis and characters for the status bar
 # 💎 💻 💡 🔌 ⚡ 📁 \|
-echo "$user@$host | $uptime | $battery % ($bat0_status) | $date_formatted"
+echo "$user@$host | $uptime | n/a % ($bat0_status) | $date_formatted"
 
